23a6dbdea4bcb32facb1365e0e11ddb4925870a4
kitt <catgirl@catgirlin.space>
2023-04-11 11:09:18 -0400

    Update admin view for users

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as AbstractUserAdmin
+
 from .models import User
+
+
+class UserAdmin(AbstractUserAdmin):
+    def __init__(self, model, admin_site):
+        super().__init__(model, admin_site)
+        self.fieldsets[1][1]["fields"] = ("display_name", "email")
+
 
 admin.site.register(User, UserAdmin)
