b0ea00b799bb72557e3142a50ff3833cabb0ddd2
kitt <catgirl@catgirlin.space>
2023-04-20 13:54:07 -0400

    use splatoon3.ink images

--- splatnet_assets/management/commands/updatestages.py
+++ splatnet_assets/management/commands/updatestages.py
@@ -1,16 +1,20 @@
 import requests
 from django.core.management import BaseCommand
 
-from splatnet_assets.management.commands._private import download_image_from_path
+from splatnet_assets.management.commands._private import download_image_from_path, download_image
 from splatnet_assets.models import Stage, LocalizationString
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         stage_data = requests.get('https://leanny.github.io/splat3/data/mush/310/VersusSceneInfo.json').json()
+        splatoon_3_ink_data = requests.get('https://splatoon3.ink/data/stages.json').json()
+        splatoon_3_ink_data = splatoon_3_ink_data['data']['stageRecords']['nodes']
+        splatoon_3_ink_data = {stage['stageId']: stage for stage in splatoon_3_ink_data}
+
         for stage in stage_data:
             if stage['__RowId'][:3] == 'Vss':
-                # remove any numbers from the rowid
+                data = splatoon_3_ink_data[stage['Id']]
                 image_id = ''.join([c for c in stage['__RowId'] if not c.isdigit()])
 
                 Stage.objects.update_or_create(
@@ -21,10 +25,10 @@ class Command(BaseCommand):
                             type=LocalizationString.Type.STAGE,
                             internal_id=''.join([c for c in stage['__RowId'][4:] if not c.isdigit()]),
                         )[0],
-                        'image': download_image_from_path(
+                        'image': download_image(
                             'stage',
                             image_id,
-                            f'stageL/{image_id}.png',
+                            data['originalImage']['url'],
                         ),
                         'image_banner': download_image_from_path(
                             'stage_banner',
