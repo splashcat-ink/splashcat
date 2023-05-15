3d39144a2a5d3fd6fe44f0011bcb3f8e9daeb29f
kitt <catgirl@catgirlin.space>
2023-05-15 01:10:02 -0400

    add b2 storage

--- splashcat/settings.py
+++ splashcat/settings.py
@@ -186,7 +186,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STORAGES = {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
+STORAGES = global_settings.STORAGES | {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
 AWS_S3_ACCESS_KEY_ID = os.environ.get('B2_ACCESS_KEY_ID')
 AWS_S3_SECRET_ACCESS_KEY = os.environ.get('B2_SECRET_ACCESS_KEY')
 AWS_S3_REGION_NAME = 'us-west-004'
