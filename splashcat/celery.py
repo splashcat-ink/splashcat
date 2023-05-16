4b6280abbeac9763d4cb7e46b62813f65ad4add3
kitt <catgirl@catgirlin.space>
2023-05-16 08:40:43 -0400

    that isnt the fix...

--- splashcat/celery.py
+++ splashcat/celery.py
@@ -14,7 +14,7 @@ app = Celery('splashcat')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
-app.autodiscover_tasks(force=True)
+app.autodiscover_tasks()
 
 
 @app.task(bind=True)
