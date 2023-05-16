de6b270f19ee1dfb939fb9a987b98896975d5c07
kitt <catgirl@catgirlin.space>
2023-05-16 11:18:56 -0400

    fix form

--- splashcat/settings.py
+++ splashcat/settings.py
@@ -186,7 +186,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STORAGES = global_settings.STORAGES | {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
+if not DEBUG:
+    STORAGES = global_settings.STORAGES | {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
 AWS_S3_ACCESS_KEY_ID = os.environ.get('B2_ACCESS_KEY_ID')
 AWS_S3_SECRET_ACCESS_KEY = os.environ.get('B2_SECRET_ACCESS_KEY')
 AWS_S3_REGION_NAME = 'us-west-004'
