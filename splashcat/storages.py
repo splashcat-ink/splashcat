6f0edc28887896af42f9f347217b07b58d2ed089
kitt <catgirl@catgirlin.space>
2023-06-02 20:59:50 -0400

    Revert "switch to storing static on b2"
    This reverts commit d48fe9698791f4b6d3ae1bc4fbd8cd909a8fd811.
    
    doing this because django-storages is currently very slow to deploy :/

--- splashcat/storages.py
+++
@@ -1,6 +0,0 @@
-from storages.backends.s3boto3 import S3ManifestStaticStorage
-
-
-class StaticStorage(S3ManifestStaticStorage):
-    bucket_name = 'splashcat-static'
-    custom_domain = 'static.splashcat.ink'
