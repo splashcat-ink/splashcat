eac969ad5646939a54e0587063cd3b1df66e778e
kitt <catgirl@catgirlin.space>
2023-08-12 01:45:09 -0400

    does anyone read these?

--- search/views.py
+++ search/views.py
@@ -9,8 +9,7 @@ from battles.models import Player, Battle
 @login_required
 def search_for_players_played_with(request, npln_id: str):
     players = Player.objects.filter(team__battle__uploader_id=request.user.id, npln_id__iexact=npln_id).distinct()
-    battles_with_player = (Battle.objects.with_prefetch().filter(teams__players__in=players).distinct()
-                           .order_by('-played_time'))
+    battles_with_player = Battle.objects.filter(teams__players__in=players).distinct().order_by('-played_time')
 
     return render(request, 'search/search_for_players_played_with.html', {
         'battles': battles_with_player,
