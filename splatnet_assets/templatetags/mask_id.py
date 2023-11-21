f0730c910593cb99f286036c59003ab16e7452ce
kitt <catgirl@catgirlin.space>
2023-11-21 12:17:14 -0500

    random color things fix

--- splatnet_assets/templatetags/mask_id.py
+++ splatnet_assets/templatetags/mask_id.py
@@ -1,6 +1,9 @@
+import random
+
 from django import template
 from django.utils.crypto import get_random_string
 
+from splatnet_assets.fields import Color
 from splatnet_assets.models import SubWeapon, SpecialWeapon
 from users.models import User, SponsorshipTiers
 
@@ -13,13 +16,19 @@ def mask_id(weapon_object: SubWeapon | SpecialWeapon):
     return f"mask_id_{weapon_object.id}_{random_string}"
 
 
+random_color_options = [
+    Color.from_hex("cd43a6"),
+]
+
+
 @register.simple_tag(takes_context=True)
-def get_color(context, uploader: User):
+def get_color(context, uploader: User, use_random_color: bool):
     user = context.get('user')
     if uploader.sponsor_tiers[SponsorshipTiers.SPONSOR]:
         context['color'] = uploader.favorite_color
     elif user and user.is_authenticated:
         context['color'] = context['user'].favorite_color
     if not context.get('color'):
-        context['color'] = uploader.battles.latest("played_time").teams.first().color
+        context['color'] = random.choice(random_color_options) if use_random_color else uploader.battles.latest(
+            "played_time").teams.first().color
     return ''
