169b15a572926c4e9f116698395e7b969c636f21
Rosalina <catgirl@catgirlin.space>
2024-02-14 09:26:08 -0500

    fix link verification

--- users/tasks.py
+++ users/tasks.py
@@ -23,17 +23,10 @@ def validate_rel_me_links(user_id):
         link.is_rel_me_verified = False
         html = requests.get(link.url).content
         bs4 = BeautifulSoup(html, "html.parser")
-        a_links = bs4.find_all("a[rel=me]")
-        for a_link in a_links:
-            if a_link.get("href") == profile_url:
+        links = bs4.find_all(["a", "link"], rel="me")
+        for link_element in links:
+            if link_element.get("href") == profile_url:
                 link.is_rel_me_verified = True
                 break
-
-        if not link.is_rel_me_verified:
-            link_links = bs4.find_all("link[rel=me]")
-            for link_link in link_links:
-                if link_link.get("href") == profile_url:
-                    link.is_rel_me_verified = True
-                    break
 
         link.save()
