319711250dddfe37aeb928a2d077e32698e8341e
Rosalina <catgirl@catgirlin.space>
2024-02-28 18:06:44 -0500

    more graphql work

--- users/types.py
+++ users/types.py
@@ -1,10 +1,20 @@
-import strawberry_django
-from strawberry import auto
+from typing import List
 
+import strawberry.relay
+import strawberry_django
+from strawberry import auto, relay
+from strawberry_django.relay import ListConnectionWithTotalCount
+
+from battles.types import Battle
 from . import models
 
 
 @strawberry_django.type(models.User)
-class User:
-    id: auto
+class User(relay.Node):
     username: auto
+    saved_favorite_color: auto
+    display_name: auto
+    preferred_pronouns: auto
+    x_battle_division: auto
+    profile_picture: auto
+    battles: ListConnectionWithTotalCount[Battle] = strawberry_django.connection()
