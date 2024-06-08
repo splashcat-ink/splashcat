ec582a44d9044132dc5265e2430b883172cc63c3
Rosalina <catgirl@catgirlin.space>
2024-06-08 18:03:48 -0400

    add a __str__ method to AlbumImage

--- splatnet_album/models.py
+++ splatnet_album/models.py
@@ -14,3 +14,6 @@ class AlbumImage(models.Model):
     width = models.IntegerField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"@{self.user.username} - {self.npln_image_id}"
